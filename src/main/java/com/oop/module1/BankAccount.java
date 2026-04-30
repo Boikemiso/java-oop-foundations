21f83258cd3b5a69d23b6f802225024b8a1df126
Bongani-Xhentsa <mqondokamtho@gmail.com>
2026-04-30 17:10:20 +0200

    Add Module 1 Exercise 2: BankAccount Class

---
+++ src/main/java/com/oop/module1/BankAccount.java
@@ -0,0 +1,42 @@
+package com.oop.module1;
+
+/**
+ * Exercise 2: BankAccount Class
+ *
+ * TODO: Create a BankAccount class with the following requirements:
+ * 1. Fields:
+ *    - accountNumber (String)
+ *    - balance (double)
+ *    - owner (String)
+ *
+ * 2. Constructor:
+ *    - A constructor that accepts accountNumber, owner, and initialBalance
+ *    - Should initialize all three fields
+ *
+ * 3. Methods:
+ *    - getBalance() - returns the current balance
+ *    - getAccountNumber() - returns the account number
+ *    - getOwner() - returns the owner name
+ *    - deposit(double amount) - adds amount to balance, throws IllegalArgumentException if amount <= 0
+ *    - withdraw(double amount) - subtracts amount from balance, throws IllegalArgumentException if amount <= 0 or amount > balance
+ *    - toString() - returns a String representation like "BankAccount{accountNumber='123456', owner='John', balance=1500.0}"
+ *
+ * WHY: This exercise teaches you how to:
+ * - Create methods that modify object state
+ * - Use methods to perform operations
+ * - Validate inputs in methods
+ * - Maintain invariants (balance should never be negative)
+ */
+public class BankAccount {
+    // TODO: Add fields here
+    
+    // TODO: Add constructor here
+    
+    // TODO: Add getter methods here
+    
+    // TODO: Add deposit method here
+    
+    // TODO: Add withdraw method here
+    
+    // TODO: Override toString() here
+}
