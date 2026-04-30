bac232c029ae19058ea58c6f05e51d328cafef59
Bongani-Xhentsa <mqondokamtho@gmail.com>
2026-04-30 17:08:43 +0200

    Add Module 1 Exercise 1: Person Class

---
+++ src/main/java/com/oop/module1/Person.java
@@ -0,0 +1,36 @@
+package com.oop.module1;
+
+/**
+ * Exercise 1: Person Class
+ *
+ * TODO: Create a Person class with the following requirements:
+ * 1. Fields:
+ *    - name (String)
+ *    - age (int)
+ *    - email (String)
+ *
+ * 2. Constructor:
+ *    - A constructor that accepts name, age, and email
+ *    - Should initialize all three fields
+ *
+ * 3. Methods:
+ *    - getAge() - returns the age
+ *    - getName() - returns the name
+ *    - getEmail() - returns the email
+ *    - toString() - returns a String representation like "Person{name='John', age=30, email='john@example.com'}"
+ *
+ * WHY: This exercise teaches you how to:
+ * - Define fields that represent object state
+ * - Create constructors to initialize objects
+ * - Use getters to access object data
+ * - Override toString() for meaningful object representation
+ */
+public class Person {
+    // TODO: Add fields here
+    
+    // TODO: Add constructor here
+    
+    // TODO: Add getter methods here
+    
+    // TODO: Override toString() here
+}
